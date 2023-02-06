eb85ac2975fd6fb07583b4548b7ef97c245bf5e9
Janek Winkler <janekx21@gmail.com>
2023-02-06 17:16:47 +0100

    Preparing for v5

---
+++ src/Evergreen/Migrate/V5.elm
@@ -0,0 +1,103 @@
+module Evergreen.Migrate.V5 exposing (..)
+
+import Evergreen.V4.Types as Old
+import Evergreen.V5.Types as New
+import Lamdera.Migrations exposing (..)
+
+
+migrateLayout layout =
+    case layout of
+        Old.Neo ->
+            New.Neo
+
+        Old.NeoQwertz ->
+            New.NeoQwertz
+
+        Old.Bone ->
+            New.Bone
+
+        Old.AdNW ->
+            New.AdNW
+
+        Old.KOY ->
+            New.KOY
+
+        Old.NeoQwerty ->
+            New.NeoQwerty
+
+        Old.Vou ->
+            New.Vou
+
+        Old.Mine ->
+            New.Mine
+
+
+migrateSettings : Old.Settings -> New.Settings
+migrateSettings settings =
+    { blockOnError =
+        case settings.blockOnError of
+            Old.OneBackspace ->
+                New.OneBackspace
+
+            Old.CorrectLetter ->
+                New.CorrectLetter
+    , fontSize = settings.fontSize
+    , paddingLeft = settings.paddingLeft
+    , paddingRight = settings.paddingRight
+    , layout = migrateLayout settings.layout
+    , theme = New.ElectricFields
+    }
+
+
+defaultAuth : New.AuthModel
+defaultAuth =
+    { username = "", failed = New.NotAsked, password = "", passwordVisibility = False }
+
+
+migrateStatistic : Old.PastDictation -> New.PastDictation
+migrateStatistic old =
+    { errors = old.errors
+    , lesson = old.lesson
+    , duration = old.duration
+    , finished = old.finished
+    }
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    let
+        migrated =
+            { key = old.key
+            , page = New.AuthPage defaultAuth
+            , usersCount2 = 1
+            , settings = migrateSettings old.settings
+            , statistic = List.map migrateStatistic old.statistic
+            , authorised = old.authorised
+            }
+    in
+    ModelMigrated ( migrated, Cmd.none )
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelUnchanged
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgUnchanged
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgOldValueIgnored
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgOldValueIgnored
