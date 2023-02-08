3522cb7beb4646f0bd3e68aaa6a2d9ff80d0e435
Janek Winkler <janekx21@gmail.com>
2023-02-08 15:41:18 +0100

    Preparing for v6

---
+++ src/Evergreen/Migrate/V6.elm
@@ -0,0 +1,142 @@
+module Evergreen.Migrate.V6 exposing (..)
+
+import Dict
+import Evergreen.V5.Types as Old
+import Evergreen.V6.Types as New
+import Lamdera.Migrations exposing (..)
+
+
+migrateLayout layout =
+    case layout of
+        Old.Neo ->
+            New.Neo
+
+        Old.NeoQwertz ->
+            New.NeoQwertz
+
+        Old.Bone ->
+            New.Bone
+
+        Old.AdNW ->
+            New.AdNW
+
+        Old.KOY ->
+            New.KOY
+
+        Old.NeoQwerty ->
+            New.NeoQwerty
+
+        Old.Vou ->
+            New.Vou
+
+        Old.Mine ->
+            New.Mine
+
+
+migrateTheme : Old.Theme -> New.ThemeName
+migrateTheme theme =
+    case theme of
+        Old.WheatField ->
+            New.WheatField
+
+        Old.ElectricFields ->
+            New.ElectricFields
+
+        Old.CandyLand ->
+            New.CandyLand
+
+
+migrateSettings : Old.Settings -> New.Settings
+migrateSettings settings =
+    { blockOnError =
+        case settings.blockOnError of
+            Old.OneBackspace ->
+                New.OneBackspace
+
+            Old.CorrectLetter ->
+                New.CorrectLetter
+    , fontSize = settings.fontSize
+    , paddingLeft = settings.paddingLeft
+    , paddingRight = settings.paddingRight
+    , layout = migrateLayout settings.layout
+    , theme = New.Theme (migrateTheme settings.theme) True
+    }
+
+
+defaultAuth : New.AuthModel
+defaultAuth =
+    { username = "", failed = New.NotAsked, password = "", passwordVisibility = False }
+
+
+migrateStatistic : Old.PastDictation -> New.PastDictation
+migrateStatistic old =
+    { errors = old.errors
+    , lesson = old.lesson
+    , duration = old.duration
+    , finished = old.finished
+    }
+
+
+migrateFrontend : Old.FrontendModel -> New.FrontendModel
+migrateFrontend old =
+    { key = old.key
+    , page = New.AuthPage defaultAuth
+    , usersCount2 = 1
+    , settings = migrateSettings old.settings
+    , statistic = List.map migrateStatistic old.statistic
+    , authorised = old.authorised
+    }
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelMigrated ( migrateFrontend old, Cmd.none )
+
+
+migrateUser : Old.User -> New.User
+migrateUser user =
+    { username = user.username
+    , passwordHash = user.passwordHash
+    , passwordSalt = user.passwordSalt
+    , settings = migrateSettings user.settings
+    , pastDictations = user.pastDictations
+    }
+
+
+migrateSession : Old.Session -> New.Session
+migrateSession session =
+    { user = migrateUser session.user, created = session.created }
+
+
+migrateBackend : Old.BackendModel -> New.BackendModel
+migrateBackend o =
+    { currentTime = o.currentTime
+    , passiveUsers = o.passiveUsers |> Dict.map (always migrateUser)
+    , activeSessions = o.activeSessions |> Dict.map (always migrateSession)
+    , currentSaltIndex = o.currentSaltIndex
+    }
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelMigrated ( migrateBackend old, Cmd.none )
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgOldValueIgnored
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgOldValueIgnored
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgOldValueIgnored
---
+++ src/Evergreen/V6/Types.elm
@@ -0,0 +1,227 @@
+module Evergreen.V6.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Dict
+import Lamdera
+import Time
+import Url
+
+
+type alias Lesson =
+    { title : String
+    , content : String
+    }
+
+
+type alias Menu =
+    { current : Maybe Lesson
+    }
+
+
+type alias Dictation =
+    { prev : String
+    , current : Char
+    , next : String
+    }
+
+
+type alias TypeError =
+    { was : Char
+    , should : Char
+    }
+
+
+type alias TypingModel =
+    { dictation : Dictation
+    , madeError : Bool
+    , errors : List TypeError
+    , layer : Int
+    , lesson : Lesson
+    , duration : Float
+    , paused : Bool
+    , showKeyboard : Bool
+    }
+
+
+type alias PastDictation =
+    { errors : List TypeError
+    , lesson : Lesson
+    , duration : Float
+    , finished : Time.Posix
+    }
+
+
+type alias Bucket =
+    List PastDictation
+
+
+type alias Hover =
+    Bucket
+
+
+type LoginFail
+    = NotAsked
+    | WrongUsernameOrPassword
+    | UsernameOrPasswordInvalid
+
+
+type alias AuthModel =
+    { username : String
+    , password : String
+    , passwordVisibility : Bool
+    , failed : LoginFail
+    }
+
+
+type Page
+    = MenuPage Menu
+    | TypingPage TypingModel
+    | TypingStatisticPage PastDictation
+    | SettingsPage
+    | StatisticPage Hover
+    | AuthPage AuthModel
+    | InfoPage
+
+
+type WaitingFor
+    = OneBackspace
+    | CorrectLetter
+
+
+type Layout
+    = Neo
+    | NeoQwertz
+    | Bone
+    | AdNW
+    | KOY
+    | NeoQwerty
+    | Vou
+    | Mine
+
+
+type ThemeName
+    = WheatField
+    | ElectricFields
+    | CandyLand
+
+
+type alias Theme =
+    { name : ThemeName
+    , dark : Bool
+    }
+
+
+type alias Settings =
+    { blockOnError : WaitingFor
+    , fontSize : Int
+    , paddingLeft : Int
+    , paddingRight : Int
+    , layout : Layout
+    , theme : Theme
+    }
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , page : Page
+    , settings : Settings
+    , statistic : List PastDictation
+    , authorised : Bool
+    , usersCount2 : Int
+    }
+
+
+type alias Username =
+    String
+
+
+type alias User =
+    { username : Username
+    , passwordHash : String
+    , passwordSalt : String
+    , settings : Settings
+    , pastDictations : List PastDictation
+    }
+
+
+type alias Session =
+    { user : User
+    , created : Time.Posix
+    }
+
+
+type alias BackendModel =
+    { currentSaltIndex : Int
+    , passiveUsers : Dict.Dict Username User
+    , activeSessions : Dict.Dict Lamdera.SessionId Session
+    , currentTime : Time.Posix
+    }
+
+
+type KeyboardKey
+    = Character Char
+    | Control String
+
+
+type TypingMsg
+    = KeyDown KeyboardKey
+    | KeyUp KeyboardKey
+    | TickTypingTime
+    | Pause
+    | Play
+    | ToggleKeyboard
+    | Exit
+
+
+type AuthMsg
+    = SetUsername String
+    | SetPassword String
+    | SetVisibility Bool
+    | TryLogin String String
+    | TryRegister String String
+    | ToInfo
+
+
+type PageMsg
+    = TypingMsg TypingMsg
+    | AuthMsg AuthMsg
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | PageMsg PageMsg
+    | Back
+    | SetSettings Settings
+    | OnHover Hover
+    | Logout
+    | FinishedDictation (List TypeError) Lesson Float Time.Posix
+    | ChangePage Page
+
+
+type ToBackend
+    = UpdateSettings Settings
+    | GetSettings
+    | InsertUser Username String
+    | InsertSession Username String
+    | GetSession
+    | RemoveSession
+    | ConsStatistic PastDictation
+    | GetStatistic
+    | GetUserCount
+
+
+type BackendMsg
+    = TimeTick Time.Posix
+
+
+type ToFrontend
+    = GotSettings Settings
+    | KickOut
+    | RegisterFailed
+    | LoginSuccessful
+    | LoginFailed
+    | UpdateStatistic (List PastDictation)
+    | UpdateUserCount Int
