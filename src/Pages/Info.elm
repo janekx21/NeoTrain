de8a442210af59aa619ca6f14ed915c4ef0fd88a
Janek Winkler <janekx21@gmail.com>
2023-08-31 00:21:45 +0200

    mod info text

--- src/Pages/Info.elm
+++ src/Pages/Info.elm
@@ -13,10 +13,13 @@ view userCount t =
         , paragraph [] [ text "Neo ist eine ergonomische Tastaturbelegung, welche für die deutsche Sprache optimiert ist. Wenn du noch mehr über Neo erfahren möchstes besuch bitte die Homepage." ]
         , el [ padding 16, centerX ] <| link (buttonAttributes t) { url = "https://www.neo-layout.org/", label = text "Neo Homepage" }
         , subTitle "Über Mich"
-        , paragraph [] [ text "Hallo ich bin Janek aus Magdeburg. Ich studiere Informatik an der OvGU und programmiere super gerne in Elm. Habe diese Web-App aus Spaß gebaut." ]
-        , row [ centerX ]
-            [ el [ padding 16, centerX ] <| link (buttonAttributes t) { url = "https://github.com/janekx21/Neotrain", label = text "Github Repository" }
-            , el [ padding 16, centerX ] <| link (buttonAttributes t) { url = "https://github.com/janekx21", label = text "Mein Github" }
+        , paragraph [] [ text "Ich bin Janek, studiere Informatik an der OvGU und programmiere super gerne in Elm. Ich habe diese Web-App aus Libe zu Neo und Elm gebaut." ]
+        , column [ centerX ]
+            [ row [ centerX ]
+                [ el [ padding 16 ] <| link (buttonAttributes t) { url = "https://github.com/janekx21/Neotrain", label = text "Github Repository" }
+                , el [ padding 16 ] <| link (buttonAttributes t) { url = "https://github.com/janekx21", label = text "Mein Github" }
+                ]
+            , el [ padding 16, centerX ] <| link (buttonAttributes t) { url = "https://github.com/janekx21/NeoTrain/issues/new", label = text "Problem melden" }
             ]
         , subTitle "Über Neo Train"
         , paragraph [] [ text "Die Web-Anwendung ist dafür gedacht Neo über Diktate zu lernen. Fang am besten mit der obersten Lektion an und arbeite dich nach unten weiter." ]
